c9d9975231ee707de020c2ff78c31307b6f09f4f
임형주 <hjlim4u@naver.com>
2021-11-09 11:32:11 +0900

    string method

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const os = require('os');
 const fs = require('fs');
 
 const folder = process.argv[2];
-// console.log(process.argv);
-// console.log(os.homedir());
-// fs.readdir(os.homedir()).then(console.log);
+//작업폴더 설정
 const workingDir = path.join(os.homedir(),'Pictures', folder);
-// console.log(workingDir);
+//입력한 디렉토리가 존재하지 않을 경우 에러
+
 if (!folder || !fs.existsSync(workingDir)) {
     console.error('input a folder name in Pictures');
     return;
@@ -16,10 +15,12 @@ const videoDir = path.join(workingDir, 'video');
 const capturedDir = path.join(workingDir, 'captured');
 const dupliDir = path.join(workingDir, 'duplicated');
 
+//파일 디렉토리 생성
 !fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
 !fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
 !fs.existsSync(dupliDir) && fs.mkdirSync(dupliDir);
 
+//작업폴더안의 파일들 분류
 fs.promises.readdir(workingDir).then(files=>{
     files.forEach(file=>{
         if(file.endsWith('.mp4') || file.endsWith('.mov')){
