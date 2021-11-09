c4ba4a3d3846f49fe0dc61a11ad430d0df0aa822
임형주 <hjlim4u@naver.com>
2021-11-09 11:29:13 +0900

    switch case

--- appSwitch.js
+++ appSwitch.js
@@ -3,8 +3,9 @@ const os = require('os');
 const fs = require('fs');
 
 const folder = process.argv[2];
-
+//작업폴더 설정
 const workingDir = path.join(os.homedir(),'Pictures', folder);
+//입력한 디렉토리가 존재하지 않을 경우 에러
 if (!folder || !fs.existsSync(workingDir)) {
     console.error('input a folder name in Pictures');
     return;
@@ -13,10 +14,12 @@ const videoDir = path.join(workingDir, 'video');
 const capturedDir = path.join(workingDir, 'captured');
 const dupliDir = path.join(workingDir, 'duplicated');
 
+//파일 디렉토리 생성
 !fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
 !fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
 !fs.existsSync(dupliDir) && fs.mkdirSync(dupliDir);
 
+//작업폴더안의 파일들 분류
 fs.promises.readdir(workingDir).then(files=>{
     files.forEach(file=>{
         switch (path.extname(file)){
