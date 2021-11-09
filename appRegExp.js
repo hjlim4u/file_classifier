9d936a842045c6da7672cc2b80468aebde86d1f1
임형주 <hjlim4u@naver.com>
2021-11-09 11:23:52 +0900

    regular expression

--- appRegExp.js
+++ appRegExp.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 
 const folder = process.argv[2];
 
+//작업폴더 설정
 const workingDir = path.join(os.homedir(),'Pictures', folder);
-
+//입력한 디렉토리가 존재하지 않을 경우 에러
 if (!folder || !fs.existsSync(workingDir)) {
     console.error('input a folder name in Pictures');
     return;
@@ -13,11 +14,12 @@ if (!folder || !fs.existsSync(workingDir)) {
 const videoDir = path.join(workingDir, 'video');
 const capturedDir = path.join(workingDir, 'captured');
 const dupliDir = path.join(workingDir, 'duplicated');
-
+//파일 디렉토리 생성
 !fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
 !fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
 !fs.existsSync(dupliDir) && fs.mkdirSync(dupliDir);
 
+//작업폴더안의 파일들 분류
 fs.promises.readdir(workingDir).then(files=>{
     files.forEach(file=>{
         if(isVideoFile(file)){
@@ -34,7 +36,7 @@ fs.promises.readdir(workingDir).then(files=>{
     })
 })
 
-
+//특정 파일을 해당 디렉토리로 옮김
 function move(file, directory){
     console.info(`move ${file} to ${directory}`);
     const oldpath = path.join(workingDir, file);
